2e1200bcb31bde88b19c0a22d2105421677b0574
Yessiest <yessiest@protonmail.com>
2020-11-23 13:15:51 +0400

    Added some commands and a terminal controller

--- pulseboard.py
+++ pulseboard.py
@@ -2,6 +2,7 @@ from pynput import keyboard
 import vlc
 import re
 import random
+import asyncio
 print("Instantiating the player")
 instance = vlc.Instance()
 micspam_player = instance.media_player_new()
@@ -19,51 +20,88 @@ vlc.libvlc_audio_output_device_list_release(mods)
 print("Select audio device: ")
 iter = 0
 for devname in devices:
-    iter+=1
     print(str(iter)+") "+devname.decode("utf-8"))
+    iter+=1
 device_id = int(input())
-while device_id < 0 or device_id > len(devices):
-    print("No such device")
-    device_id = int(input())
+while device_id < 0 or device_id > len(devices)-1:
+   print("No such device")
+   device_id = int(input())
+global device
 device = devices[device_id]
 #define some functions for help
+mode = ""
 def exists(dic,key):
     try:
         return dic[key]
     except:
         return False
-def exec(comm):
-    print(comm)
-    if comm.startswith("play"):
-        filename = re.match("play (.+)",comm).group(1)
-        micspam_player.audio_output_device_set(None,device)
-        micspam_player.set_mrl(filename)
-        micspam_player.play()
-    if comm.startswith("debug"):
-        print("Debug test")
-    if comm.startswith("test"):
-        raise StopException()
 class InputHandler:
     def __init__(self):
         self.keymap = {}
         self.inputbuffer = []
     def on_press(self,key):
         self.inputbuffer.append(key)
-        if exists(self.keymap,str(self.inputbuffer)):
-            exec(self.keymap[str(self.inputbuffer)])
+        if exists(self.keymap,mode+"#"+str(self.inputbuffer)):
+            exec(self.keymap[mode+"#"+str(self.inputbuffer)])
     def on_release(self,key):
         try:
             self.inputbuffer.remove(key)
         except:
             self.inputbuffer.clear()
     def bind(self,keymap,comm):
-        name = keyboard.HotKey.parse(keymap)
-        self.keymap[str(name)] = comm
+        keybind = re.match("(.*?)#?(.+)",keymap)
+        print(keybind.group(1),keybind.group(2))
+        kmode = keybind.group(1) or ""
+        name = keyboard.HotKey.parse(keybind.group(2))
+        self.keymap[kmode+"#"+str(name)] = comm
     def unbind(self,keymap):
         name = keyboard.HotKey.parse(keymap)
-        if exists(self.keymap,str(name)):
-            self.keymap.pop(str(name))
+        if exists(self.keymap,mode+"#"+str(name)):
+            self.keymap.pop(mode+"#"+str(name))
+    def list(self):
+        for k,v in self.keymap.items():
+           print(k+": "+v)
+
 kmap = InputHandler()
+def exec(comm):
+    global device
+    if comm.startswith("play "):
+        filename = re.match("play (.+)",comm).group(1)
+        micspam_player.set_mrl(filename)
+        micspam_player.audio_output_device_set(None,device)
+        micspam_player.play()
+    if comm.startswith("stop"):
+        micspam_player.stop()
+    if comm.startswith("mode"):
+        mode = re.match("mode(.*)",comm).group(1)
+        mode = mode.lstrip()
+    if comm.startswith("list"):
+        kmap.list()
+    if comm.startswith("device-list"):
+        dev_id = 0
+        for I in devices:
+            print(dev_id+") "+I.decode("utf-8"))
+            dev_id+=1
+    if comm.startswith("device-set "):
+        dev_id = re.match("device-set (\d+)",comm).group(1)
+        device = devices[int(dev_id)]
+    if comm.startswith("help"):
+        print("""
+Commands:
+help - print this message
+play <filename> - play the sound to the currently set device
+stop - stop the sound
+mode <mode> - set the bind mode
+list - list all bindings
+device-list - list all devices
+device-set - set the output device
+exit - exit the program
+
+bindings should be set via ./.pulsebrc file in this form:
+[mode#<shift>+k]="command"
+where <shift>+k is the keybinding you wish to set (special keys should be wrapped in <>)
+and command is the command to execute
+        """)
 try:
     rc = open("./.pulsebrc")
     text = rc.read()
@@ -72,5 +110,13 @@ try:
 except IOError:
     print("No pulsebrc found - creating")
     rc = open("./.pulsebrc","w")
+    exit()
+running = True
 with keyboard.Listener(on_press=kmap.on_press,on_release=kmap.on_release) as listener:
-    listener.join()
+    while running:
+        term_comm = input("> ")
+        if term_comm.startswith("exit"):
+            running = False
+        else:
+            exec(term_comm)
+
